8b521723ba7eb4fab4208691bcf5945871cab370
Elton Whybrow <whybrow.elton@gmail.com>
2017-02-13 15:38:50 +0000

    About you page styling

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1>About</h1>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>Weather application build for the purpose of gaining a better understanding of React for the bootcamp</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweatehrmap.org">Open Weather Map</a> - This was the open source weather API used in this example.
+        </li>
+        <li>
+          <a href="https://eltonwhybrow.github.io/">Angular2 - Recipe book app</a> - This was the Angular 2 app created and pushed github pages in my other tutorial.
+        </li>
+      </ul>
     </div>
   )
 };
